6e684fc0acb55c9054454577852d4905a0af75e4
Alexandru Frangulea <a.frangulea@gmail.com>
2022-08-11 16:25:18 +0300

    html + css + initialize the game

---
+++ src/buttons.js
@@ -0,0 +1,26 @@
+import { ICONS } from "./constants";
+
+const toggleHighlited = (icon, show) =>
+  document
+    .querySelector(`.${ICONS[icon]}-icon`)
+    .classList.toggle("highlited", show);
+
+export default function initButtons(handleUserAction) {
+  let selectedIcon = 0;
+
+  function buttonClick({ target }) {
+    if (target.classList.contains("left-btn")) {
+      toggleHighlited(selectedIcon, false);
+      selectedIcon = (2 + selectedIcon) % ICONS.length;
+      toggleHighlited(selectedIcon, true);
+    } else if (target.classList.contains("right-btn")) {
+      toggleHighlited(selectedIcon, false);
+      selectedIcon = (1 + selectedIcon) % ICONS.length;
+      toggleHighlited(selectedIcon, true);
+    } else {
+      handleUserAction(ICONS[selectedIcon]);
+    }
+  }
+
+  document.querySelector(".buttons").addEventListener("click", buttonClick);
+}
